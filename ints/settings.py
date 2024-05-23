8ff0460204dddb4550912ac931a34f0cbec12684
michaelamangubat0416 <mangubat.m.bsinfotech@gmail.com>
2024-05-23 10:19:01 +0800

    kath

--- ints/settings.py
+++ ints/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'ints.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306', #run xampp
+
     }
 }
 
